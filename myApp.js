cff19df7047bc555a3bb32679a13a5468d1d12b7
Matt <mattmleach@gmail.com>
2019-06-17 15:07:59 -0400

    added post

--- myApp.js
+++ myApp.js
@@ -47,6 +47,12 @@ app.get('/name', (req, res) => {
     });
 });
 
+app.post('/name', (req, res) => {
+    res.json({
+        name: `${ req.body.first} ${req.body.last }`
+    });
+});
+
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
  module.exports = app;
