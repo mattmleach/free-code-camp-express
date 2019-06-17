d66ca256cd5c14012a3f25bf2f3295af6e0b0f0c
Matt <mattmleach@gmail.com>
2019-06-17 14:09:15 -0400

    updated time middle

--- myApp.js
+++ myApp.js
@@ -21,6 +21,12 @@ app.get("/json", (req, res) => {
     
 });
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res, next) =>{
+    res.json({ time: req.time });
+});
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
