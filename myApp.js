1f192f059208eb7e1b8777b38f64591448f435b4
Matt <mattmleach@gmail.com>
2019-06-17 12:56:33 -0400

    added static

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
