e415baca4a405ecbeed491ac99343a1aef5d1fb2
Matt <mattmleach@gmail.com>
2019-06-17 13:57:11 -0400

    update

--- myApp.js
+++ myApp.js
@@ -2,12 +2,12 @@ var dotenv = require('dotenv').config();
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
-
 app.use((req, res, next) => {
     console.log(req.method + req.path + req.ip);
     next();
 });
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
