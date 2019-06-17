abd00447ae0dfb049d0815441efd0eeddfcae82f
Matt <mattmleach@gmail.com>
2019-06-17 13:58:37 -0400

    updated format

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use((req, res, next) => {
-    console.log(req.method + req.path + req.ip);
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
 
