478f0f491124908e7b26d6e460041e9ea3ae4c47
Matt <mattmleach@gmail.com>
2019-06-17 13:56:06 -0400

    added middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+    console.log(req.method + req.path + req.ip);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
