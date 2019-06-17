b9614a88dae60aac542ed30541c17c50074fd9aa
Matt <mattmleach@gmail.com>
2019-06-17 13:03:36 -0400

    added environment case

--- myApp.js
+++ myApp.js
@@ -9,7 +9,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-    res.json({ "message": "Hello json" });
+
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+
+    }
+    const message = process.env.MESSAGE_STYLE === 'uppercase' 
+        ? "HELLO JSON" 
+        : "Hello Json";
+
+    res.json({ "message": message });
 });
 
 
