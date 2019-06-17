6ce4362eeb45a950b8d8fda849780565b1d5cf24
Matt <mattmleach@gmail.com>
2019-06-17 13:41:36 -0400

    remove .env

--- myApp.js
+++ myApp.js
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
 
 app.get("/json", (req, res) => {
 
+    console.log(process.env);
+
     const message = process.env.MESSAGE_STYLE === "uppercase" 
         ? "HELLO JSON" 
         : "Hello Json";
